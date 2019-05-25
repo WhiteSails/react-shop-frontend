da684f08130751b024e285ca36dd697a980403a9
midsummer <jelena@fedurtsya.com>
2019-05-25 22:07:00 +0300

    layout fixed

--- src/components/actions/actions.js
+++ src/components/actions/actions.js
@@ -1,43 +1,42 @@
 import React, {Component} from "react";
 
 class Actions extends Component {
-   constructor(props) {
-       super(props);
-       this.state = {
-           count: 1
-       };
-   }
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 1
+        };
+    }
 
     increment() {
-       this.setState({
-           count: this.state.count +1
-       });
+        this.setState({
+            count: this.state.count + 1
+        });
     };
 
-   decrement() {
-       if(this.state.count <= 1) {
-           return
-       }
-       this.setState({
-           count: this.state.count -1
-       });
-   };
+    decrement() {
+        if (this.state.count <= 1) {
+            return
+        }
+        this.setState({
+            count: this.state.count - 1
+        });
+    };
 
-   add() {
-       this.props.addToCart(this.props.productId, this.state.count);
-   }
-   render() {
+    add() {
+        this.props.addToCart(this.props.productId, this.state.count);
+    }
 
+    render() {
         return (
             <div className="float-right">
                 <button className="btn btn-light" onClick={(e) => this.decrement(e)}>-</button>
                 <input type="text" value={this.state.count} readOnly/>
                 <button className="btn btn-light" onClick={(e) => this.increment(e)}>+</button>
                 <button className="add_button ml-2 btn btn-outline-info" onClick={() => this.add()}>
-               Add
+                    Add
                 </button>
             </div>
-
         )
     }
 }
